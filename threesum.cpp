95a839d67e788f8cd0fe9796c5a963f9b4d5e8a2
hariiiiiiiii <haricharan006@gmail.com>
2025-10-20 08:42:45 +0530

    Three Sum

---
+++ threesum.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = nums.size() - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) k--;
+                else if (total < 0) j++;
+                else {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> ans = sol.threeSum(nums);
+
+    cout << "[";
+    for (int i = 0; i < ans.size(); i++) {
+        cout << "[";
+        for (int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j];
+            if (j < ans[i].size() - 1) cout << ",";
+        }
+        cout << "]";
+        if (i < ans.size() - 1) cout << ",";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
